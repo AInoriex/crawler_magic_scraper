ef61fcdce35b9f29eba956a5a56fa0febd200190
AInoriex <xiangyuhao@52tt.com>
2024-08-19 14:58:41 +0800

    [feat] add language to scrape;

--- ytb_scrape_arg.py
+++ ytb_scrape_arg.py
@@ -6,14 +6,21 @@ from time import sleep
 from ytb_scrape import scrape_pipeline
 
 def main():
-    if len(argv) <= 1:
-        print("[ERROR] too less arguments of urls to scrape.")
+    if len(argv) <= 2:
+        print("[ERROR] Too less arguments of urls to scrape.")
+        print("[INFO] Example: python ytb_scrape.py yue https://www.youtube.com/@video-df1md")
         exit()
     pid = getpid()
-    for url in argv[1:]:
-        print(f"[INFO] now scrape url:{url}")
-        sleep(1)
-        scrape_pipeline(pid, channel_url=url)
+    language = argv[1]
+    opt = input(f"[DEBUG] Check your input, language:{language}, url:{argv[2:]}. Continue?(Y/N)")
+    if opt in ["Y", "y", "YES", "yes"]:
+        for url in argv[2:]:
+            print(f"[INFO] Now scrape url:{url}")
+            sleep(1)
+            scrape_pipeline(pid, channel_url=url, language=language)
+    else:
+        print(f"You input {opt}. Bye!")
+        exit()
 
 if __name__ == "__main__":
     main()
